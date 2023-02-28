dd94184a8f0b3fbdb92f597eb1fd11bcf31cc1c6
Жалгасова Сания Асланбековна <ZhalgasovaS@lcoast.itstep>
2023-02-28 14:40:18 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Post from './Post.vue'
+    export default {
+        components:{
+            Post
+        },
+        data() {
+            return {
+                amount: 1
+            }
+        },
+        methods: {
+            addPost(){
+                this.amount++
+            }
+        }
+
+    }
+</script>
+
+<template>
+    <input type="button" value="Add post" @click="addPost">
+    <Post v-for="n in amount" :key="n"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/Post.vue
@@ -0,0 +1,71 @@
+<script>
+export default {
+    data() {
+        return {
+            post: {
+                text: "",
+                backColor: "white",
+                fontColor: "black",
+                fontSize: "20px",
+                fontStyle: "normal",
+                fontWeight: "500",
+                fontFamily: "Arial",
+                disable: true
+            }
+        }
+    },
+    methods: {
+        disableChange(){
+            this.post.disable = !this.post.disable
+            console.log(this.post)
+        },
+        changeFont (event){
+            this.post.fontSize = event.target.value + "px"
+        }
+    }
+}
+</script>
+
+
+<template>
+      <div class="post">
+        <textarea :disabled="post.disable" class="postText" :style="{backgroundColor: post.backColor, color: post.fontColor, fontSize: post.fontSize, fontWeight: post.fontWeight, fontStyle: post.fontStyle, fontFamily: post.fontFamily}" name="test" id="" cols="30" rows="10" v-model="post.data"></textarea>
+        <input @click="disableChange" type="button" value="Редактировать">
+        <div class="changeInputs" :class="{visible: post.disable}">
+            <p>Background color</p>
+            <input type="color" name="" id="" v-model="post.backColor">
+            <p>Font color</p>
+            <input type="color" name="" id="" v-model="post.fontColor">
+            <p>Font size</p>
+            <input type="number" name="" id="" @input="changeFont" value="14">
+            <p>Font weight</p>
+            <input type="number" name="" id="" v-model="post.fontWeight">
+            <p>Font style</p>
+            <select name="" id="" v-model="post.fontStyle">
+                <option value="normal">normal</option>
+                <option value="italic">italic</option>
+                <option value="oblique">oblique</option>
+            </select>
+            <p>Font family</p>
+            <select name="" id="" v-model="post.fontFamily">
+                <option value="Arial">Arial</option>
+                <option value="Georgia">Georgia</option>
+                <option value="Verdana">Verdana</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<style>
+    .post{
+        border: 1px solid rgb(28, 91, 226);
+        width: 500px;
+    }
+    .changeInputs{
+        border: 1px solid black;
+        width: 200px;
+    }
+    .visible{
+        display: none;
+    }
+</style>
